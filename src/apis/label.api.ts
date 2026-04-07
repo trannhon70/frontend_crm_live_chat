098fdb9f1d260719e20ddfaf6902af3a07119dd6
trannhon70 <kevintran351996@gmail.com>
2026-04-07 16:20:47 +0700

    update

--- src/apis/label.api.ts
+++ src/apis/label.api.ts
@@ -7,38 +7,38 @@ export const labelAPI = {
     getById,
     update,
     getAll,
-    
+
 };
 
 
 async function create(body: any) {
-    const response = await instance.post(`/label/create`,body);
+    const response = await instance.post(`/labels/create`, body);
     return response.data
 }
 
 async function getPaging(query: any) {
-    const response = await instance.get(`/label/get-paging?pageIndex=${query.pageIndex}&pageSize=${query.pageSize}&search=${query.search}`);
+    const response = await instance.get(`/labels/get-paging?pageIndex=${query.pageIndex}&pageSize=${query.pageSize}&search=${query.search}`);
     return response.data.data
 }
 
 async function deletes(id: any) {
-    const response = await instance.delete(`/label/delete/${id}`);
+    const response = await instance.delete(`/labels/delete/${id}`);
     return response.data
 }
 
 async function getById(id: any) {
-    const response = await instance.get(`/label/get-by-id/${id}`);
+    const response = await instance.get(`/labels/get-by-id/${id}`);
     return response.data
 }
 
-async function update(id:number, body: any) {
-    const response = await instance.put(`/label/update/${id}`,body);
+async function update(id: number, body: any) {
+    const response = await instance.put(`/labels/update/${id}`, body);
     return response.data
 }
 
 
 
 async function getAll() {
-    const response = await instance.get(`/label/get-all`);
+    const response = await instance.get(`/labels/get-all`);
     return response.data
 }
